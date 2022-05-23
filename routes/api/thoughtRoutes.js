5a0f506e674abd1a046893ab797504e7843a729d
danivelarde <danielavelarde4@gmail.com>
2022-05-22 20:37:41 -0400

    apiroutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const{
+    getAllThoughts,
+    addThought,
+    getThoughtById,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require ('../../controllers/thoughtController');
+const {route} = require('./userRoutes');
+
+router.route('/').get(getAllThoughts);
+
+router.route('/:userId').post(addThought);
+
+router
+.route('/:thoughtId')
+.get(getThoughtById)
+.put(updateThought)
+
+router
+
+.route('/:userId/:thoughtId')
+.delete(removeThought);
+
