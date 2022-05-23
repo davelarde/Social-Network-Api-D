11a7ff8c4e54ce0126ad2394faf68dff26b22d60
danivelarde <danielavelarde4@gmail.com>
2022-05-22 21:22:03 -0400

    controllers error

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,22 @@ const userController ={
             res.status(400).json(err);
         });
     },
+
+    getUserById({params},res){
+        User.findOne({_id:params.id})
+        .populate('thoughts')
+        .populate('friends')
+        .select('__v')
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({msg: ' No user found with this Id'});
+                return ;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
 }
