75d4f42e96d8a45e39a440ffb5fc346de1ed1e2e
danivelarde <danielavelarde4@gmail.com>
2022-05-22 21:46:53 -0400

    usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,5 +48,49 @@ const userController ={
         })
         .catch(err => res.status(400).json(err));
     },
-    
+    // to delete user
+
+    deleteUser({params},res){
+        User.findOneAndDelete({_id:params.id})
+        .then(dbUserData =>{
+            if(!dbUserData){
+                return res.status(404).json({msg:'No user with this Id'});
+
+            }
+
+        })
+        .then(()=>{
+            res.json({msg:'This user has been deleted'});
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // to add a friend
+
+    addFriend({params}, res){
+        User.findOneAndUpdate({_id: params.id},{$addToSet: { friends: params.friendId}},{runValidators:true})
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({msg:'No user found with this id'});
+                return ;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // remove friend
+
+    removeFriend({params}, res){
+        User.findOneAndUpdate({_id: params.id}, {$pull: { friends: params.friendId}}, {runValidators: true})
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({msg:'No user found with this id'});
+                return;  
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
 }
+module.exports = userController;
