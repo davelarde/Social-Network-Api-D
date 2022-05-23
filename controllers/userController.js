2e66cd9eae7250f5af9d4ca20a0081e4bf1fd217
danivelarde <danielavelarde4@gmail.com>
2022-05-22 21:28:07 -0400

    route to update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const {User,Thought} = require('../models');
-
+// get all users
 const userController ={
     getAllUsers(req, res){
         User.find({})
@@ -9,7 +9,7 @@ const userController ={
             res.status(400).json(err);
         });
     },
-
+// get user by id
     getUserById({params},res){
         User.findOne({_id:params.id})
         .populate('thoughts')
@@ -27,4 +27,26 @@ const userController ={
             res.status(400).json(err);
         });
     },
+
+    // this one is to create a user
+
+    createUser({body},res){
+        User.create(body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    // to update by id
+    updateUser({params,body},res){
+        User.findOneAndUpdate({_id:params.id},body,{new: true, runValidators: true})
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({msg:'No user has this Id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    
 }
